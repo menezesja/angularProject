ad39f7a90d7ddab9601f8860c2ccab1a295a6398
menezesja <santmjad@gmail.com>
2023-09-18 14:43:31 -0400

    test

--- ola-mundo/src/app/app.component.spec.ts
+++ ola-mundo/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ola-mundo'`, () => {
+  it(`should have as title 'Hello Wordl!'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ola-mundo');
+    expect(app.title).toEqual('Hello Wordl!');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ola-mundo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Hello Wordl!');
   });
 });
