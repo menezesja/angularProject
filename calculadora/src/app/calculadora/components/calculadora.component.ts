68aa6b89a4e885c18dbc9ce7fc776711e3fe0229
menezesja <santmjad@gmail.com>
2023-09-19 10:22:16 -0400

    fix: calculadora.component.ts

--- calculadora/src/app/calculadora/components/calculadora.component.ts
+++ calculadora/src/app/calculadora/components/calculadora.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CalculadoraService } from '../services';
 
 @Component({
   selector: 'app-calculadora',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculadoraComponent implements OnInit {
 
-  constructor(){ }
+  //serviços do componente, criação de metodos
+  constructor(private calculadoraService: CalculadoraService) { }
 
   ngOnInit() {
     
