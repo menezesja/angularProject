21b5ad51fd76502ec0d346660c4c63defd19d5e2
menezesja <santmjad@gmail.com>
2023-09-19 09:50:26 -0400

    add services calculadora

--- calculadora/src/app/calculadora/calculadora.module.ts
+++ calculadora/src/app/calculadora/calculadora.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { CalculadoraComponent } from './components';
 
 
 
@@ -10,6 +10,11 @@ import { CalculadoraComponent } from './calculadora/calculadora.component';
   ],
   imports: [
     CommonModule
+  ],
+
+  exports: [
+    CalculadoraComponent
   ]
+
 })
 export class CalculadoraModule { }
