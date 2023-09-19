4b842d075a2bd10c28ec3ac1d1cd1ea4592bdecd
menezesja <santmjad@gmail.com>
2023-09-19 14:04:13 -0400

    feat: metodos para realizar os calculos

--- calculadora/src/app/calculadora/services/calculadora.service.ts
+++ calculadora/src/app/calculadora/services/calculadora.service.ts
@@ -4,6 +4,35 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CalculadoraService {
+  //Contantes utilizadas para identificação das operações de cálculo
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly MULTIPLICACAO: string = '*';
+  static readonly DIVISAO: string = '/';
 
   constructor() { }
+  //metodo para calcular uma operacao de dois números
+  //let cria uma várivael local
+  calcular(num1: number, num2: number, operacao: string): number {
+    let resultado: number; //armazena o resultado da operação (typescript)
+
+    switch(operacao) {
+      case CalculadoraService.SOMA:
+        resultado = num1 + num2;
+      break;
+      case CalculadoraService.SUBTRACAO:
+        resultado = num1 - num2;
+      break;
+      case CalculadoraService.MULTIPLICACAO:
+        resultado = num1 * num2;
+      break;
+      case CalculadoraService.DIVISAO:
+        resultado = num1 / num2;
+      break;
+      default:
+        resultado = 0; 
+    }
+    return resultado;
+  }
+
 }
