6cfd0fb525a79ec98a13fda1b958d1fb940c2f27
menezesja <santmjad@gmail.com>
2023-09-29 08:15:07 -0400

    feat: adicionada module e component

--- jogo-da-velha/src/app/app.component.spec.ts
+++ jogo-da-velha/src/app/app.component.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { JogoDaVelhaModule } from './jogo-da-velha';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent]
+    declarations: [AppComponent],
+    imports: [JogoDaVelhaModule]
   }));
 
   it('should create the app', () => {
@@ -12,16 +14,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'jogo-da-velha'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('jogo-da-velha');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('jogo-da-velha app is running!');
-  });
 });
