838d437193b9171a6a18aa740557dd68e59c4a1e
menezesja <santmjad@gmail.com>
2023-10-09 09:16:38 -0400

    feat: module conversor de moedas

--- conversor-de-moedas/src/app/app.module.ts
+++ conversor-de-moedas/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ConversorModule } from './conversor';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ConversorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
