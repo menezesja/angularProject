3ad78af9ed4abcf338f39f3dd67e5b31bd8b6271
menezesja <santmjad@gmail.com>
2023-10-10 09:24:37 -0400

    feat: implemtação formulário de conversão de moedas

--- conversor-de-moedas/src/app/conversor/components/conversor.component.ts
+++ conversor-de-moedas/src/app/conversor/components/conversor.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component , OnInit, ViewChild} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Moeda, Conversao, ConversaoResponse } from '../models';
+import { MoedaService, ConversorService } from '../services';
+
 
 @Component({
   selector: 'app-conversor',
   templateUrl: './conversor.component.html',
   styleUrls: ['./conversor.component.css']
 })
-export class ConversorComponent {
+export class ConversorComponent implements OnInit{
 
+  moedas: Moeda[];
+  conversao: Conversao;
+  possuiErro: boolean;
+  conversaoResponse: ConversaoResponse;
+
+  @ViewChild("conversaoForm", { static: true }) conversaoForm: NgForm;
+
+  constructor(
+    private moedaService: MoedaService,
+    private conversorService: ConversorService) {}
+
+  ngOnInit(){
+    this.moedas = this.moedaService.listarTodas();
+    this.init();
+  }
+
+  init(): void{
+    this.conversao = new Conversao('USD', 'BRL', null);
+    this.possuiErro = false;
+  }
+
+  converter(): void{
+    if (this.conversaoForm.form.valid){
+      alert('Convertendo: ' + JSON.stringify(this.conversao));
+    }
+  }
 }
--- conversor-de-moedas/src/app/conversor/conversor.module.ts
+++ conversor-de-moedas/src/app/conversor/conversor.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http'; 
+import { FormsModule } from '@angular/forms';
 import { ConversorComponent } from './components';
 import { MoedaService, ConversorService } from './services';
 
@@ -10,7 +11,8 @@ import { MoedaService, ConversorService } from './services';
   ],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   exports: [
     ConversorComponent
