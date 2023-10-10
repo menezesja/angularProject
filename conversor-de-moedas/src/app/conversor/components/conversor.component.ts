8dd2583b1871248e756ae008ac97e52e5aa48915
menezesja <santmjad@gmail.com>
2023-10-10 12:03:30 -0400

    feat: implementação ação de conversão de moedas

--- conversor-de-moedas/src/app/conversor/components/conversor.component.ts
+++ conversor-de-moedas/src/app/conversor/components/conversor.component.ts
@@ -2,7 +2,7 @@ import { Component , OnInit, ViewChild} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Moeda, Conversao, ConversaoResponse } from '../models';
 import { MoedaService, ConversorService } from '../services';
-
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-conversor',
@@ -34,7 +34,21 @@ export class ConversorComponent implements OnInit{
 
   converter(): void{
     if (this.conversaoForm.form.valid){
-      alert('Convertendo: ' + JSON.stringify(this.conversao));
+      this.conversorService.converter(this.conversao).pipe(
+        catchError((err: any) => {
+          this.possuiErro = true;
+          return of(null); // Retorne um valor padrão ou vazio aqui, se necessário
+        })
+      )
+      .subscribe((response) => {
+        if (response) {
+          this.conversaoResponse = response;
+        }
+      });
     }
+      /*this.conversaoResponse = JSON.parse(
+        `{"base":"${this.conversao.moedaDe}", "date":"2023-10-10", "rates":{"${this.conversao.moedaPara}": 5.4321}}`
+      );
+    };*/
   }
 }
