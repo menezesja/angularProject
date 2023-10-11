a1c158347c5cbdaf1d6c34c64d5ad18ced9d585d
menezesja <santmjad@gmail.com>
2023-10-11 10:39:24 -0400

    feat: implementação do dashboard

--- dashboard/src/app/dashboard/dados.service.ts
+++ dashboard/src/app/dashboard/dados.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class DadosService {
-
+  
+  readonly dados = [
+    ['Janeiro', 33],
+    ['Fevereiro', 68],
+    ['Março', 49],
+    ['Abril', 15],
+    ['Maio', 80],
+    ['Junho', 27]
+  ]
   constructor() { }
+
+  //Retorna um observable contendo os dados a serem exibidos no gráfico.
+  obterDados(): Observable<any>{
+    return new Observable(observable => {
+      observable.next(this.dados); 
+      observable.complete();
+    })
+  }
 }
