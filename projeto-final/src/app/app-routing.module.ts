251308148abf808ddafb07ccf83dc8d1447b3a27
menezesja <santmjad@gmail.com>
2023-10-13 09:54:38 -0400

    feat: adicionada rotas do módulo Dashboard

--- projeto-final/src/app/app-routing.module.ts
+++ projeto-final/src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+ 
+import { DashboardRoutes } from './dashboard';
+ 
 export const routes: Routes = [
-   /* {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-    }*/
+	{ 
+		path: '', 
+		redirectTo: '/dashboard', 
+		pathMatch: 'full' 
+	},
+	...DashboardRoutes
 ];
-
+ 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
