9092462ca8d36550b4abfdded5983c2a7f68a8be
menezesja <santmjad@gmail.com>
2023-10-13 09:33:37 -0400

    feat: html do menu principal projeto-final

--- projeto-final/src/app/app.module.ts
+++ projeto-final/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'; 
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
